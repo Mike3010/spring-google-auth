7a0c8333e47946fa0ca3f8865acad22d0bb21411
Mike Logan <mike3010@gmail.com>
2020-09-07 11:17:32 +0200

    Added cross origin settings for angular

--- src/main/java/com/example/googleauth/GoogleAuthApplication.java
+++ src/main/java/com/example/googleauth/GoogleAuthApplication.java
@@ -3,7 +3,7 @@ package com.example.googleauth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,17 +14,19 @@ import java.security.Principal;
 @RestController
 public class GoogleAuthApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(GoogleAuthApplication.class, args);
 	}
 
- 	@GetMapping("/test")
+	@CrossOrigin(origins = "http://localhost:4200")
+	@GetMapping("/test")
 	public String test() {
 		return "test works";
 	}
 
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/user")
-	public Principal currentUserName(Principal principal) {
+	public Principal currentUserName(final Principal principal) {
 		return principal;
 	}
 }
